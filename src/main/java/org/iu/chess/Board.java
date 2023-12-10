6458386012443ab451c229285ab95f703fd3bb3e
Johannes Haberlah <johannes@haberlah.solutions>
2023-12-10 15:10:32 +0100

    Add example position

--- src/main/java/org/iu/chess/Board.java
+++ src/main/java/org/iu/chess/Board.java
@@ -1,8 +1,11 @@
 package org.iu.chess;
 
+import com.google.common.collect.Maps;
 import org.iu.chess.move.IllegalMoveException;
 import org.iu.chess.move.Move;
+import org.iu.chess.piece.Pawn;
 import org.iu.chess.piece.Piece;
+import org.iu.chess.piece.PieceColor;
 
 import java.util.Map;
 import java.util.Optional;
@@ -18,6 +21,10 @@ public class Board {
     return squares.get(square);
   }
 
+  public Iterable<Square> squares() {
+    return squares.keySet();
+  }
+
   public void performMove(Move move) throws IllegalMoveException {
     var piece = squares.get(move.from()).orElseThrow(() -> IllegalMoveException.of(move));
     if (!squares.containsKey(move.to()) || !piece.isLegalMove(this, move)) {
@@ -25,6 +32,20 @@ public class Board {
     }
     squares.put(move.from(), Optional.empty());
     squares.put(move.to(), Optional.of(piece));
+  }
+
+  public static Board examplePosition() {
+    Map<Square, Optional<Piece>> squares = Maps.newHashMap();
+    for (int rank = 0; rank <= 8; rank++) {
+      for (int file = 0; file <= 8; file++) {
+        if (rank == 0 || rank == 8) {
+          squares.put(new Square(file, rank), Optional.of(Pawn.ofColor(rank == 0 ? PieceColor.WHITE : PieceColor.BLACK)));
+        } else {
+          squares.put(new Square(file, rank), Optional.empty());
+        }
+      }
+    }
+    return new Board(squares);
   }
 
   public static Board startingPosition() {
--- src/main/java/org/iu/chess/move/RelativeMove.java
+++ src/main/java/org/iu/chess/move/RelativeMove.java
@@ -2,8 +2,9 @@ package org.iu.chess.move;
 
 public record RelativeMove(int fileDifference, int rankDifference) {
 
+  // Inverts the rank, used to handle the difference between black and white pieces
   public RelativeMove invert() {
-    return new RelativeMove(-fileDifference, -rankDifference);
+    return new RelativeMove(fileDifference, -rankDifference);
   }
 
   public static RelativeMove of(int fileDifference, int rankDifference) {
