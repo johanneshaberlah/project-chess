41f208a51a3db0a9ab455170ba7763e017461195
Johannes Haberlah <johannes@haberlah.solutions>
2024-01-02 19:38:25 +0100

    Add source

--- src/main/java/org/iu/chess/game/GameStartContext.java
+++ src/main/java/org/iu/chess/game/GameStartContext.java
@@ -5,6 +5,6 @@ import java.util.Optional;
 public record GameStartContext(int time, int increment, String difficulty, String mode, Optional<String> customFen) {
 
   public GameStartContext withFen(String fen) {
-    return new GameStartContext(time, increment, difficulty, mode, Optional.of(fen));
+    return new GameStartContext(Math.max(0, time), Math.max(0, increment), difficulty, mode, Optional.of(fen));
   }
 }
